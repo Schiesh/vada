745431d419884209d8e2ea4cb59f0da53625c42d
Schiesh <russell.schiesser@gmail.com>
2021-12-04 21:21:18 -0500

    Update main Delete voice.mp3

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import os
 import time
-import playsound
+from datetime import date
+from pygame import mixer
 import speech_recognition as sr
 from gtts import gTTS
 
+bot_message = ""
+
 def speak(text):
-    tts = gTTS(text=text, lang="en")
-    filename = "voice.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
+    mixer.init()
+    tempText = text.replace(" ", "_")
+    filename = tempText + ".mp3"
+    if os.path.exists(filename):
+        mixer.music.load(filename)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(1)
+        mixer.quit()
+
+    else:
+        tts = gTTS(text=text, lang="en")    
+        tts.save(filename)
+        mixer.music.load(filename)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(1)
+        mixer.quit()
 
 def get_audio():
     r = sr.Recognizer()
@@ -18,16 +35,37 @@ def get_audio():
 
         try:
             said = r.recognize_google(audio)
-            print(said)
+            print(said.lower())
         except Exception as e:
-            print("Exception "+ str(e))
+            print("Did not recognize input "+ str(e))
 
-    return said
+    return said.lower()
 
-text = get_audio()
+WAKE = "okay veda"
+print("Start")
 
-if "hello" in text:
-    speak("Hello, how are you?")
+while True:
+    print("Listening")
+    message = get_audio()
 
-if "what is your name" in text:
-    speak("My name is Veda")
+    if message.count(WAKE) > 0:
+        speak("How may I assist you")
+        message = get_audio()
+
+        if "hello" in message:
+            speak("Hello how are you")
+
+        if "your name" in message:
+            speak("My name is Veda")
+
+        if "today's date" in message:
+            today = date.today()
+            date = today.strftime("%B %d %Y")
+            speak("Today is " + date)
+            dateTemp = date.replace(" ", "_")
+            os.remove("Today_is_" + dateTemp + ".mp3")
+        
+        if "bye" in message:
+            speak("Have a nice day")
+            speak("Good bye")
+            bot_message = "Bye"
